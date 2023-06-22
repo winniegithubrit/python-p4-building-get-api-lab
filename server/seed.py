f55ae08d3af5fd040ccd186ffa264c36fa86abe6
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-06-22 17:23:27 +0300

    Completed work

--- server/seed.py
+++ server/seed.py
@@ -10,7 +10,7 @@ from models import db, Bakery, BakedGood
 fake = Faker()
 
 with app.app_context():
-    db.create_all()  # Create the database tables if they don't exist
+    db.create_all() 
 
     BakedGood.query.delete()
     Bakery.query.delete()
